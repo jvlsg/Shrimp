05ee633c2dbcc8ef1f0ddfefd6a66396103f66fd
jvlsg <jvlsg@users.noreply.github.com>
2022-01-16 16:56:08 -0300

    UPD Many changest to expand
    Still figuring best approach to handling multiline input

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -1,83 +1,123 @@
-use std::{env, fs, path};
+use std::{env, fs, path, io};
 
 use dirs;
 
-enum ExpansionError {
-    PairNotFound,
+//POSSIBLY - CHANGE EXPANSION ERROR FOR ACTUAL ERRORS. HAVE A ENUM FOR THE OK STATUSES THAT THE INPUT HANDLER WILL NEED TO HANDLE
+pub enum ExpansionError {
+    PairNotFound(char),
+    EnvVarError
 }
 
-// TODO switch to result
-pub fn expand(input: &str) -> Vec<String> {
-    let mut split_input: Vec<String> = Vec::with_capacity(input.len()); //Worst case scenario, each char is whitespace separated
-    let mut temp_buffer = String::with_capacity(input.len());
+pub fn read_input() -> Vec<String> {
 
-    let mut chars = input.chars().peekable();
+    //TODO modularize this in a "InputHandler"
+    let mut input_raw = String::new();
 
-    while let Some(c) = chars.next() {
+    let mut split_input = vec![];
+
+    io::stdin().read_line(&mut input_raw).unwrap();
+
+    loop {
+    match expand(&input_raw, &mut split_input){
+        //CURRENT PROBLEM - IF THERE'S AN UNCLOSED TERMINATOR (PAIR NOT FOUND), WE NEED TO READ MORE UNTIL WE FIND A TERMINATOR. AT THE SAME TIME, WHEN WE READ THE NEXT LINE, WE NEED TO STORE THE LAST STATUS, I.E. WE NEED TO KNOW THAT WE'LL BE LOOKING FOR THE PAIR. OR IN THE CASE OF LINES ENDING WITH `\` UNTIL THERE'S A LINE WITHOUT `\`
+
+        //WHAT IF WE ENCAPSULATE THIS LOGIC INTO A "INPUT HANDLER" THAT WILL LOOP UNTIL IT GETS A OK(DONE) FROM THE PREPROCESSOR, AND STORES THE LAST RESULTS IN A STACK OF SOME SORT TO HANDLE EXCEPTIONS?
+
+        //E.G. input handler reads line, passes to preprocessor which finds only one `'` and returns OK(PairNotFound(')). Input processor reads next line and feeds it to the preprocessor. But ideally it would load it straight into the Singlequote expansion. Should we pass the last result as an argument? Or the "llast results" stack?
+
+        //What if we get the mainline from the preprocessor into the input handler main loop? 
+        Ok(_) => {break},
+        // Err(ExpansionError::PairNotFound(c)) => {
+        //     //
+        // }
+        _ => {
+            dbg!("Erro");
+            ()
+        }
+    }
+    }
+    split_input
+}
+
+///Handles expansions / metacharacters the user can input on a line.
+pub fn expand(input_raw: &str, input_processed: &mut Vec<String>) -> Result<(),ExpansionError> {
+    
+    let mut split_input: Vec<String> = Vec::with_capacity(input_raw.len()); //Worst case scenario, each char is whitespace separated
+    let mut expanded_buffer = String::with_capacity(input_raw.len());
+
+    let mut leftover_string = String::new();
+    let mut input_buffer = input_raw.chars().peekable();
+
+    while let Some(c) = input_buffer.next() {
         match c {
             '$' => {
-                expand_env_var(&mut chars, &mut temp_buffer);
+                expand_env_var(&mut input_buffer, &mut expanded_buffer);
             }
             '*' | '?' | '[' => { //Expand until the next non-special character
-                 // expand_pathname(&mut chars, &mut temp_buffer);
+                 // expand_pathname(&mut chars, &mut expanded_buffer);
             }
             '~' => {
                 if let Some(home) = dirs::home_dir() {
-                    temp_buffer.push_str(home.to_str().unwrap_or_default());
+                    expanded_buffer.push_str(home.to_str().unwrap_or_default());
                 }
                 //TODO else, log?
             }
-            '\'' => {
-                single_quote_supression(&mut chars, &mut temp_buffer);
-            }
+            // '\'' => {
+            //     let leftover_stringo = single_quote_supression(&mut input_buffer, &mut expanded_buffer);
+            //     if leftover_stringo.is_some(){
+            //         leftover_string = leftover_stringo.unwrap();
+            //         input_buffer = leftover_string.chars().peekable()
+            //     }
+            // }
             '\"' => {
-                double_quote_supression(&mut chars, &mut temp_buffer);
+                double_quote_supression(&mut input_buffer, &mut expanded_buffer);
             }
             '\\' => {
 
             }
             _ if c.is_whitespace() => {
-                dbg!(&temp_buffer);
+                dbg!(&expanded_buffer);
                 //We don't want to clone a String with the same capacity, this will allocate only the needed space
-                split_input.push(temp_buffer.as_str().to_string());
-                temp_buffer.clear();
+                split_input.push(expanded_buffer.as_str().to_string());
+                expanded_buffer.clear();
             }
             _ => {
-                temp_buffer.push(c);
+                expanded_buffer.push(c);
             }
         }
     }
 
     //sanity checking
-    if !temp_buffer.is_empty() {
-        // expanded_input.push_str(&temp_buffer);
-        split_input.push(temp_buffer.as_str().to_string());
+    if !expanded_buffer.is_empty() {
+        // expanded_input.push_str(&expanded_buffer);
+        split_input.push(expanded_buffer.as_str().to_string());
     }
 
     dbg!(&split_input);
-    split_input
+    input_processed.append(&mut split_input);
+    Ok(())
 }
 
-fn expand_env_var(chars: &mut std::iter::Peekable<std::str::Chars>, temp_buffer: &mut String) {
+fn expand_env_var(input_buffer: &mut std::iter::Peekable<std::str::Chars>, expanded_buffer: &mut String) {
     //Get var name
-    let var_name = get_next_word_whitespace_separated(chars);
+    let var_name = get_next_word_whitespace_separated(input_buffer);
     dbg!(&var_name);
     if let Ok(value) = env::var(var_name) {
-        temp_buffer.push_str(&value);
+        expanded_buffer.push_str(&value);
     }
 }
 
 //Todo
 fn expand_pathname(
-    chars: &mut std::iter::Peekable<std::str::Chars>,
-    temp_buffer: &mut String,
+    input_buffer: &mut std::iter::Peekable<std::str::Chars>,
+    expanded_buffer: &mut String,
 ) -> std::io::Result<()> {
-    //See contents of temp_buffer - if it forms a path
+    //See contents of expanded_buffer - if it forms a path
     //Get before * -
-    let mut dir_path = if temp_buffer.is_empty() {
+    let mut dir_path = if expanded_buffer.is_empty() {
         std::env::current_dir()?
     } else {
-        path::PathBuf::from(&temp_buffer)
+        path::PathBuf::from(&expanded_buffer)
     };
     //Beginning of filename, if exists
     let mut before_star = None;
@@ -92,7 +132,7 @@ fn expand_pathname(
 
     dir_path.pop();
 
-    let after_star = get_next_word_whitespace_separated(chars);
+    let after_star = get_next_word_whitespace_separated(input_buffer);
 
     dbg!(&dir_path);
     dbg!(&before_star);
@@ -118,36 +158,47 @@ fn expand_pathname(
 }
 
 /// Supresses all expansions
-fn single_quote_supression(
-    chars: &mut std::iter::Peekable<std::str::Chars>,
-    temp_buffer: &mut String,
-) -> Result<(), ExpansionError> {
-    let mut found_pair = false;
+fn single_quote_supression(){
+//     input_buffer: &mut std::iter::Peekable<std::str::Chars>,
+//     expanded_buffer: &mut String,
+// ) -> Option<String> { //It might return a next line
+    
+//     let mut found_pair = false;
+//     let mut new_input_buffer = String::new();
 
-    while let Some(c) = chars.next() {
-        match c {
-            '\'' => {
-                found_pair = true;
-                break;
-            }
-            _ => {
-                //preserve all characters including whitespace
-                temp_buffer.push(c);
-            }
-        }
-    }
+//     let mut curr_input = input_buffer;
 
-    //If not found pair, return error
-    if !found_pair {
-        return Err(ExpansionError::PairNotFound);
-    }
-    Ok(())
+//     //Reads all input, including new lines if necessary, until a pair to `'` is found
+//     while !found_pair {
+//         while let Some(c) = curr_input.next() {
+//             match c {
+//                 '\'' => {
+//                     found_pair = true;
+//                     break;
+//                 }
+//                 _ => {
+//                     //preserve all characters including whitespace
+//                     expanded_buffer.push(c);
+//                 }
+//             }
+//         }
+//         //If not found pair, return error
+//         if !found_pair {
+//             io::stdin().read_line(&mut new_input_buffer).unwrap();
+//             curr_input = new
+//         }
+
+//     }
+
+//     let leftover_string: String = curr_input.collect();
+//     if !leftover_string.is_empty() {Some(leftover_string)} else {None}
+unimplemented!();
 }
 
 /// Supresses all expansions, with the exception of $ and \ expansion
 fn double_quote_supression(
-    chars: &mut std::iter::Peekable<std::str::Chars>,
-    temp_buffer: &mut String,
+    input_buffer: &mut std::iter::Peekable<std::str::Chars>,
+    expanded_buffer: &mut String,
 ) -> Result<(), ExpansionError> {
     unimplemented!();
     // let mut found_pair = false;
@@ -160,7 +211,7 @@ fn double_quote_supression(
     //         }
     //         _ => {
     //             //preserve all characters including whitespace
-    //             temp_buffer.push(c);
+    //             expanded_buffer.push(c);
     //         }
     //     }
     // }
@@ -173,41 +224,41 @@ fn double_quote_supression(
 }
 
 
-fn get_next_word_whitespace_separated(chars: &mut std::iter::Peekable<std::str::Chars>) -> String {
+fn get_next_word_whitespace_separated(input_buffer: &mut std::iter::Peekable<std::str::Chars>) -> String {
     let mut next_word = String::new();
 
-    while let Some(c) = chars.peek() {
+    while let Some(c) = input_buffer.peek() {
         if c.is_whitespace() {
             break;
         }
-        next_word.push(chars.next().unwrap());
+        next_word.push(input_buffer.next().unwrap());
     }
     next_word
 }
 
 mod test {
     use super::*;
-    #[test]
-    fn simple_env() {
-        let key = "KEY";
-        env::set_var(key, "VALUE");
-        assert_eq!(
-            expand("echo $KEY"),
-            vec![String::from("echo"), String::from("VALUE")]
-        );
-    }
+//     #[test]
+//     fn simple_env() {
+//         let key = "KEY";
+//         env::set_var(key, "VALUE");
+//         assert_eq!(
+//             expand("echo $KEY"),
+//             vec![String::from("echo"), String::from("VALUE")]
+//         );
+//     }
 
-    #[test]
-    fn simple_pathname() {
-        // expand("my/dir/file*.txt");
-        // expand("my/dir/*.txt");
-        // expand("file*.txt");
-        // expand("*.txt");
+//     #[test]
+//     fn simple_pathname() {
+//         // expand("my/dir/file*.txt");
+//         // expand("my/dir/*.txt");
+//         // expand("file*.txt");
+//         // expand("*.txt");
 
-        // expand("../src/main*s");
-        // expand("../src/*.txt");
-        //TODO FAILING HERE
-        expand("m*.rs");
-        // expand("*.rs");
-    }
+//         // expand("../src/main*s");
+//         // expand("../src/*.txt");
+//         //TODO FAILING HERE
+//         expand("m*.rs");
+//         // expand("*.rs");
+//     }
 }
